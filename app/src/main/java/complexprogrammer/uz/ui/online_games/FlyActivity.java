2e7f2873023e1726d7b1b2cbd4a191fba71ffd56
Odiljon Gaybullaev <odiljongaybullayev@mail.ru>
2021-06-23 23:28:48 +0500

    home

---
+++ app/src/main/java/complexprogrammer/uz/ui/online_games/FlyActivity.java
@@ -0,0 +1,14 @@
+package complexprogrammer.uz.ui.online_games;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fly);
+    }
+}
